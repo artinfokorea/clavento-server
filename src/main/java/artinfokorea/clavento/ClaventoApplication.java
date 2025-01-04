c2a1ec9e3d88ea6c466be9d72df2153c6ba472c7
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2025-01-05 01:28:58 +0900

    doc: ClaventoApplication doc 옵션 추가

--- src/main/java/artinfokorea/clavento/ClaventoApplication.java
+++ src/main/java/artinfokorea/clavento/ClaventoApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {
+	@Server(url = "/", description = "Default Server URL")
+})
 @EnableJpaAuditing
 @SpringBootApplication
 public class ClaventoApplication {
